4bb3e4efb61791b47b651fcbe06475784395e7dc
Mariia Pasichna <pasichnayam@gmail.com>
2020-01-10 09:03:54 +0200

    Initial commit

---
+++ src/com/mariiapasichna/Main.java
@@ -0,0 +1,21 @@
+package com.mariiapasichna;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /*3. Написать программу которая решает квадратное уравнене вида a*x*x + b*x + c = 0, где заданы a,b,c.*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a:");
+        double a = scanner.nextDouble();
+        System.out.println("Enter b:");
+        double b = scanner.nextDouble();
+        System.out.println("Enter c:");
+        double c = scanner.nextDouble();
+
+        QuadraticEquation equation = new QuadraticEquation(a, b, c);
+        equation.getResult();
+    }
+}
---
+++ src/com/mariiapasichna/QuadraticEquation.java
@@ -0,0 +1,35 @@
+package com.mariiapasichna;
+
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void getResult() {
+        if (isQuadratic()) {
+            double discriminant = Math.pow(b, 2) - (4 * a * c);
+            if (discriminant < 0) {
+                System.out.println("The quadratic equation " + a + "*x*x + " + b + "*x + " + c + " = 0 has no real roots");
+            } else if (discriminant == 0) {
+                double x = (-1) * b / (2 * a);
+                System.out.println("The quadratic equation " + a + "*x*x + " + b + "*x + " + c + " = 0 has one root: x = " + x);
+            } else if (discriminant > 0) {
+                double x1 = ((-1) * b - Math.sqrt(discriminant)) / (2 * a);
+                double x2 = ((-1) * b + Math.sqrt(discriminant)) / (2 * a);
+                System.out.println("The quadratic equation " + a + "*x*x + " + b + "*x + " + c + " = 0 has two roots: x = " + x1 + ", x = " + x2);
+            }
+        } else {
+            System.out.println("In the quadratic equation, 'a' can't be equal 0");
+        }
+    }
+
+    private boolean isQuadratic() {
+        return a != 0;
+    }
+}
